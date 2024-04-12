5cf65c608b9fdbd6d5c1caad6384741ae80ee76d
Jang-HW <66403495+Jang-HW@users.noreply.github.com>
2024-04-12 14:30:27 +0900

    Update button.c

--- 0409/button.c
+++ 0409/button.c
@@ -11,8 +11,8 @@
 static void button_dummy(void *);
 
 static BUTTON_T gBtnObjs[] = {
-	{ USER_Btn_GPIO_Port, 	USER_Btn_Pin, 	80, 	0, 		0, 		0,		button_dummy, 	{ true, 	0 	}},
-	{ NULL, 						0, 					0,		0,		0,		0,		NULL,					{ true, 	0	}}
+	{ USER_Btn_GPIO_Port, 	USER_Btn_Pin, 	80, 	0, 		0, 	0,		button_dummy, 	{ true, 	0 	}},
+	{ NULL, 		0, 		0,	0,		0,	0,		NULL,		{ true, 	0	}}
 };
 
 void button_init(void)
